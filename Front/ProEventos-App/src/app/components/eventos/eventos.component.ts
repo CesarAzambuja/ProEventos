55dd5124b1db2484b0df275c85b926e971f1d91c
CesarAzambuja <caaso.eng@gmail.com>
2022-08-02 21:36:48 -0300

    Layout Responsivo + Forms

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -11,84 +11,10 @@ import { EventoService } from '../../services/evento.service';
   styleUrls: ['./eventos.component.scss'],
   //providers: [EventoService]
 })
+
 export class EventosComponent implements OnInit {
+  ngOnInit(): void {
 
-  modalRef?: BsModalRef;
-  public eventos: Evento[] = [];
-  eventosFiltrados: Evento[] = [];
-
-  public larguraImagem: number = 150;
-  public margemImagem: number = 2;
-  public exibirImagem: boolean = true;
-  private _filtroLista: string = '';
-
-
-  public get filtroLista(): string {
-    return this._filtroLista;
   }
 
-  public set filtroLista(value: string) {
-    this._filtroLista = value;
-    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-
-  public filtrarEventos(filtrarPor: string): Evento[] {
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-        )
-  }
-
-  isCollapsed = true;
-  constructor(
-    private eventoService: EventoService,
-    private modalService: BsModalService,
-    private toastr: ToastrService,
-    private spinner: NgxSpinnerService
-    ) {}
-
-  public ngOnInit(): void {
-    this.spinner.show();
-    this.getEventos();
-    /** spinner starts on init */
-
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-
-    }, 3000);
-  }
-
-  public alterarImagem(): void {
-    this.exibirImagem = !this.exibirImagem;
-  }
-
-  public getEventos(): void {
-
-    this.eventoService.getEventos().subscribe(
-      (_eventos: Evento[]) => {
-        this.eventos = _eventos;
-        this.eventosFiltrados = this.eventos;
-        this.spinner.hide();
-
-      },
-      (error) => {
-        this.spinner.hide();
-        this.toastr.error('Erro ao carregar os Eventos','Erro');
-
-      }
-    );
-  }
-  openModal(template: TemplateRef<any>): void {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  }
-
-  confirm(): void {
-    this.modalRef?.hide();
-    this.toastr.success('Evento Excluído com Sucesso','Excluir!');
-  }
-
-  decline(): void {
-    this.modalRef?.hide();
-  }
 }
